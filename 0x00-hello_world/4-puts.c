4cc982edac9d322622f03006539fd9be0113a957
OCHHQ <enosejecollins@gmail.com>
2022-09-09 09:17:03 -0700

    Programming is like building a multilingual puzzle

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * description: main-print out string to stdout
+ * Return: 0 if success.
+ */
+int main(void)
+{
+put("\"programming is like building a multilingual puzzle");
+return (0);
+}
