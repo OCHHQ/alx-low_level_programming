b5b7af7deacd23e471bd4029a576add3bacc4bd4
OCHHQ <enosejecollins@gmail.com>
2022-09-10 08:29:01 -0700

    git commit -m 6-size.c git commit -m

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,9 +6,9 @@
  */
 int main(void)
 {
-	printf("size of the char: %1d byte(s)\n", sizeof(char));
-	printf("size of an int: %1d byte(s)\n", sizeof(int));
-	printf("size of a long int: %1d byte(s)\n", sizeof(long int));
-	print("size of a long long int: %1d byte(s)\n", sizeof(long long int));
-	print("size of a float: %1d byte(s)\n", sizeof(float));
+	printf("size of the char: %ld byte(s)\n", sizeof(char));
+	printf("size of an int: %ld byte(s)\n", sizeof(int));
+	printf("size of a long int: %ld byte(s)\n", sizeof(long int));
+	print("size of a long long int: %ld byte(s)\n", sizeof(long long int));
+	print("size of a float: %ld byte(s)\n", sizeof(float));
 	return (0);
