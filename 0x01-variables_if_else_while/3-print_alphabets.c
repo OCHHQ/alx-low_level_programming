3475a144b2fda764dc3fbee9416a607af6811f53
OCHHQ <enosejecollins@gmail.com>
2022-09-14 04:13:56 -0700

    error: fix

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,11 +10,11 @@ int main(void)
 {
 	char low;
 
-	for (low = 'a'; low <= 'z'; low++)
-		putchar(low);
-        for (low = 'A' low <='Z'; low++)
-			putchar(low);
-	                putchar(low);
+	for(low = 'a'; low <= 'z'; low++;)
+	 	Putchar(low);
+        for(low = 'A'; low <='Z'; low++;)
+			Putchar(low);
+	                Putchar(low);
 
 			return (0);
 
