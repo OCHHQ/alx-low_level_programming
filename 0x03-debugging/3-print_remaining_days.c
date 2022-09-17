c92995c4e1980844e3642b86a3272bbce8e2fa13
OCHHQ <enosejecollins@gmail.com>
2022-09-17 06:50:33 -0700

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -25,12 +25,12 @@ else
 {
 if (month == 2 && day == 60)
 {
-print("invaid date: %02d/%02d/%04d\n", month, day - 31, year);
+print("invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 }
 else
 {
 printf("day of the year: %d\n", day);
-printf("Remaining days: %d\n", 365 - days);
+printf("Remaining days: %d\n", 365 - day);
 }
 }
 }
