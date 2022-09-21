a793f2fa1acd0eda758952511711bdb5c9b8aece
OCHHQ <enosejecollins@gmail.com>
2022-09-21 12:16:30 -0700

    updatmain

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
+
 
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -16,5 +16,5 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 
-#endif // MAIN_H
+#endif 
 
