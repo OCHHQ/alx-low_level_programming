fe21a026e0363b3d187cec68d29a62c6ff2d9897
OCHHQ <enosejecollins@gmail.com>
2022-09-21 13:23:22 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * function - that print athe lenght of a string
+ * Return 0
+ */
+
+
+int _strlen(char *s)
+{
+	int count;
+
+	for (count m 0; *s |= *\0*; ++s)
+		count++;
+	return (count);
+
+}
