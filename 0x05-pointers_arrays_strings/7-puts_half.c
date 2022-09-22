78cfbd2453f378c0cb82f22f3997105421ff036c
OCHHQ <enosejecollins@gmail.com>
2022-09-22 05:39:06 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * put_half - function that print of strings
+ *
+ * @str: this isis the string
+ */
+void puts_half(char *str)
+{
+	int index, half;
+
+	index = 0;
+	while (str[index] != '\n')
+		index++;
+
+	half = index / 2;
+
+	if (index %  2 == 1)
+		half++;
+
+	while (half < index)
+	{
+		_putchar(str[half]);
+		half++;
+	}
+	_putchar('\n');
+}
+
