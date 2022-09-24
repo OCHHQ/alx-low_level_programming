a918d62a09035d9ec66532f514c8178bf0ee8c35
OCHHQ <enosejecollins@gmail.com>
2022-09-24 07:34:46 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,10 +11,9 @@
 
 	for (index = 0; s[index] != '\0'; index++)
 		;
-	for (index = index -1; s[index] != '\0'; index--)
+	for (index = index - 1; s[index] != '\0'; index--)
 	{
 		_putchar(s[index]);
 	}
 	_putchar('\n');
 }
-
