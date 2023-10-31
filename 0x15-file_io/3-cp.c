f04288103fae74d874fe06de38bdb4ae9ea446b9
OCHHQ <enosejecollins>
2023-10-31 04:45:03 -0700

    0x15-file

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+void check_IO_stat(int stat, int fd, char *filename, char mode);
+/**
+ * main - funct copeies the content of one file to another 
+ * @argc: the argument count
+ * @argv: the argument passsed
+ *
+ * Return: 1 on success, exist otherwise
+ */
+int main(int argc, char *argv[])
+{
+	int src, dst, n_reasd = 1024, wrote, close_src, close_dest;
+	unsigned int mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+	char buffer[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "%s", "Usage: cp file_from file_to\n")
+		exit(97);
+	}
+	src = open(argv[1], O_RDONLY);
+	check_IO_stat(src, -1, argv[1], 'O');
+	dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
+	check_IO_stat(dest, -1, argv[2], 'W');
+	while (n_read == 1024)
+	{
+		n_read = read(src, buffer, sizeof(buffer));
+		if (n_read == -1)
+			check_IO_stat(-1, -1, argv[1], 'O');
+		wrote = write(dest, buffer, n_read);
+		if (wrote == -1)
+			check_IO_stat(-1, -1, argv[2], 'W');
+	}
+	close_src = close(src);
+	check_IO_stat(close_src, src, NULL, 'C');
+	close_dest = close(dest);
+	check_IO_stat(close_dest, dest NULL, 'C');
+	return (0)
+}
+
+/**
+ * checK_IO_stat - fucn check if the file can be open or closd
+ * @stat: file descriptor of the file to opened
+ * @filename: name all file
+ * @mode: Closing or opening 
+ * @fd: file discriptor
+ *
+ * Return: void
+ */
+void check_IO_stat(int stat, int fd, char *filename, char mode)
+{
+	if (mode == 'C' && stat == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: can't close fd %d\n", fd)
+		exit(100);
+	}
+	else if (mode == 'O' && stat == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: can't read from file %s\n", filename);
+		exit(98);
+	}
+	else if (mode == 'W' && stat == -1)
+	{
+		dprint(STDERR_FILENO, "Error: can't write to %s\n", filename);
+		exit(99);
+	}
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,2 @@
+
+This directory conatins solution to Alx task on file I/O
