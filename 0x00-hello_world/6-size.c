969562fd73895403ddfc597c460a381890dd68ed
OCHHQ <enosejecollins@gmail.com>
2023-12-14 06:52:37 -0800

    update6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,9 +7,7 @@
  */
 int main(void)
 {
-	/*
-	 * Print the size of various types using printf
-	 */
+	
 	printf("Size of char: %zu byte(s)\n", sizeof(char));
 	printf("Size of an int: %zu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
