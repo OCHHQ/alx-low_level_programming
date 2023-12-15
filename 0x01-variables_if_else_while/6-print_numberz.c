ff5bf3e37f57c749ad4ef563c5bf40ee8b16122f
OCHHQ <enosejecollins@gmail.com>
2023-12-15 09:51:38 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Prints single digit numbers in base 10 using putchar
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+    putchar('0');
+    putchar('1');
+    putchar('2');
+    putchar('3');
+    putchar('4');
+    putchar('5');
+    putchar('6');
+    putchar('7');
+    putchar('8');
+    putchar('9');
+
+    putchar('\n');
+
+    return (0);
+}
