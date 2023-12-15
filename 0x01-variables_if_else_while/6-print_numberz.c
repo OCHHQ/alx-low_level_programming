922486c53c704941751dd80c47fc22db6e75859d
OCHHQ <enosejecollins@gmail.com>
2023-12-15 09:57:13 -0800

    update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,18 +7,10 @@
  */
 int main(void)
 {
-    putchar('0');
-    putchar('1');
-    putchar('2');
-    putchar('3');
-    putchar('4');
-    putchar('5');
-    putchar('6');
-    putchar('7');
-    putchar('8');
-    putchar('9');
+    char digits[] = "0123456789\n";
 
-    putchar('\n');
+    putchar(digits[0]);
+    putchar(digits[1]);
 
     return (0);
 }
