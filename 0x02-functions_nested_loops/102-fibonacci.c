773f23ebd4f366f8bacfbda9ea5c3f4c55b0fcba
OCHHQ <enosejecollins@gmail.com>
2023-12-19 19:33:17 -0800

    update 102-fibonacci.c redo

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -3,15 +3,18 @@
 /**
  * Calculates the next Fibonacci number given the current two numbers.
  */
-unsigned long calculateNextFibonacci(unsigned long a, unsigned long b) {
+unsigned long calculateNextFibonacci(unsigned long a, unsigned long b)
+{
     return a + b;
 }
 
-int main(void) {
+int main(void)
+{
     int inc;
     unsigned long fib1 = 0, fib2 = 1, nextFib;
 
-    for (inc = 0; inc < 50; inc++) {
+    for (inc = 0; inc < 50; inc++)
+    {
         nextFib = calculateNextFibonacci(fib1, fib2);
 
         printf("%lu", nextFib);
