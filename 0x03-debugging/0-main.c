8dd19eaa827a1e25b84d71d5c50610fba2ab550a
OCHHQ <enosejecollins@gmail.com>
2023-12-20 05:43:01 -0800

    update main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include "main.h"
+
 /**
- * main - tests fuction if its positive or negative 
+ * main - tests function if it's positive or negative
  * Return: 0
  */
-
 int main(void)
 {
-int i;
+    int i;
 
-i = 0;
-positive_or_negative(i);
+    i = 0;
+    positive_or_negative(i);
 
-return (0);
+    return (0);
 }
