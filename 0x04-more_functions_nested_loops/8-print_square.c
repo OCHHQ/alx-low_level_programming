e9095942d0d753a0b61c044d11bc5f5d1d3fb398
OCHHQ <enosejecollins@gmail.com>
2023-12-22 01:20:26 -0800

    update 8-print_seqaure.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,23 +9,22 @@
 
 void print_square(int size)
 {
-	int incl, inc2;
+    int incl, inc2;
 
-	if (size > 0)
-	{
-		for (incl = 0; incl < (size - 1); inc1++)
-		{
-			for (inc2 = 0; inc2 < (size - 1); inc2++)
-			{
-				putchar('#');
-			}
-			
-			putchar('#');
-			putchar('\n');
-		}
-	}
-	else
-	{
-		putchar('\n');
-	}
+    if (size > 0)
+    {
+        for (incl = 0; incl < size; incl++)
+        {
+            for (inc2 = 0; inc2 < size; inc2++)
+            {
+                _putchar('#');
+            }
+
+            _putchar('\n');
+        }
+    }
+    else
+    {
+        _putchar('\n');
+    }
 }
