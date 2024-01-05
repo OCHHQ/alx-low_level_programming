afe3f82223f3e7369354440aef3ef6fb431e9abe
OCHHQ <enosejecollins@gmail.com>
2024-01-04 16:05:04 -0800

    update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,32 +1,39 @@
 #include "main.h"
+
 /**
- *cap_string  - function that capitalized the firdt 
- *@str: string to capitalize
- *Return: return the capitalized string
+ * cap_string - capitalizes the first letter of each word in a string
+ * @str: string to capitalize
+ * Return: the capitalized string
  */
 char *cap_string(char *str)
 {
-	int index = 0;
+    int index = 0;
 
-	while (str[++index])
-	{
-		while (!(str[index] >= 'a' && str[index] <= 'z'))
-			index++;
-		
-		if (str[index - 1] == ' ' ||
-				str[index - 1] == '\t' ||
-				str[index - 1] == '\n' ||
-				str[index - 1] == ',' ||
-				str[index - 1] == ';' ||
-				str[index - 1] == '.' ||
-				str[index - 1] == '!' ||
-				str[index - 1] == '?' ||
-				str[index - 1] == '"' ||
-				str[index - 1] == '(' ||
-				str[index - 1] == ')' ||
-				str[index - 1] == '{' ||
-				str[index - 1] == '}')
-			str[index] -= 32;
-	}
-	return (str);
+    while (str[index])
+    {
+        if (str[index] >= 'a' && str[index] <= 'z')
+        {
+            if (index == 0 ||
+                str[index - 1] == ' ' ||
+                str[index - 1] == '\t' ||
+                str[index - 1] == '\n' ||
+                str[index - 1] == ',' ||
+                str[index - 1] == ';' ||
+                str[index - 1] == '.' ||
+                str[index - 1] == '!' ||
+                str[index - 1] == '?' ||
+                str[index - 1] == '"' ||
+                str[index - 1] == '(' ||
+                str[index - 1] == ')' ||
+                str[index - 1] == '{' ||
+                str[index - 1] == '}')
+            {
+                str[index] -= 32;  /* Convert to uppercase */
+            }
+        }
+        index++;
+    }
+
+    return (str);
 }
+
