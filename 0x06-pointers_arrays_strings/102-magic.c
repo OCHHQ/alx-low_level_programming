ab182f85193f716e183304691361a36e90310678
OCHHQ <enosejecollins@gmail.com>
2024-01-04 16:34:39 -0800

    update 102

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -2,19 +2,22 @@
 
 int main(void)
 {
-	int n;
-	int a[5];
-	int *p;
+    int n;
+    int a[5];
+    int *p;
 
-	a[2] = 1024;
-	p = &n;
-	/*
-	 * write your line of code here..
-	 * remembe:
-	 * -you are not allowed to ...
-	 */
-	*(p + 5) = 98;
-		/*...so that line prints 98\n */
-		printf("a[2] = %d\n", a[2]);
-		return (0);
+    a[2] = 1024;
+    p = &n;
+    /*
+     * write your line of code here..
+     * remember:
+     * - you are not allowed to use a[5] = 98;
+     * - you can only use p
+     * - you can only add one line of code
+     */
+    *(p + 5) = 98; /* This line of code adds 5 to the address pointed by p and stores 98 there */
+
+    /*...so that line prints 98\n */
+    printf("a[2] = %d\n", a[2]);
+    return (0);
 }
