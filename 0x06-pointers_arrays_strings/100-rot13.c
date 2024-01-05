3931bec24b952c105f3ee24990f4caa87461705f
OCHHQ <enosejecollins@gmail.com>
2024-01-04 16:13:27 -0800

    update 100-root13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,43 +1,46 @@
 #include "main.h"
 
 /**
- * rot13 - encodes a string
- * @str:the string targeted
- * Return:
+ * rot13 - encodes a string using ROT13 substitution cipher
+ * @str: the string to be encoded
+ * Return: the encoded string
  */
-
 char *rot13(char *str)
 {
-	int index1, index2;
+    int index1, index2;
 
-	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',				
-		 									'G', 'H', 'I', 'J', 'K', 'L',
-											'M', 'N', 'O', 'P', 'Q', 'R',
-											'S', 'T', 'U', 'V', 'W', 'x',
-											'Y', 'Z', 'a', 'b', 'c', 'd',
-											'e', 'f', 'g', 'h', 'i', 'j',
-											'k', 'l', 'm', 'n', 'o', 'p',
-											'q', 'r', 's', 't', 'u', 'v',
-											'w', 'x', 'y', 'z'};
-	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
-											'T', 'U', 'V', 'W', 'X', 'Y',
-											'Z', 'A', 'B', 'C', 'D', 'E',
-											'F', 'G', 'H', 'I', 'J', 'K',
-											'L', 'M', 'N', 'o', 'p', 'q',
-											'r', 's', 't', 'u', 'v', 'w',
-											'x', 'y', 'z', 'a', 'b', 'c',
-											'd', 'e', 'g', 'h', 'i', 'j',
-											'k', 'l', 'm'};
-	while (str[++index1])
-	{
-		for (index2 = 0; index2 < 52; index2++)
-		{
-			if (str[index1] == alphabet[index2])
-			{
-				str[index1] = rot13key[index2];
-				break;
-			}
-		}
-	}
-	return (str);
+    char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+                         'G', 'H', 'I', 'J', 'K', 'L',
+                         'M', 'N', 'O', 'P', 'Q', 'R',
+                         'S', 'T', 'U', 'V', 'W', 'X',
+                         'Y', 'Z', 'a', 'b', 'c', 'd',
+                         'e', 'f', 'g', 'h', 'i', 'j',
+                         'k', 'l', 'm', 'n', 'o', 'p',
+                         'q', 'r', 's', 't', 'u', 'v',
+                         'w', 'x', 'y', 'z'};
+
+    char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+                         'T', 'U', 'V', 'W', 'X', 'Y',
+                         'Z', 'A', 'B', 'C', 'D', 'E',
+                         'F', 'G', 'H', 'I', 'J', 'K',
+                         'L', 'M', 'n', 'o', 'p', 'q',
+                         'r', 's', 't', 'u', 'v', 'w',
+                         'x', 'y', 'z', 'a', 'b', 'c',
+                         'd', 'e', 'f', 'g', 'h', 'i',
+                         'j', 'k', 'l', 'm'};
+
+    while (str[++index1])
+    {
+        for (index2 = 0; index2 < 51; index2++)
+        {
+            if (str[index1] == alphabet[index2])
+            {
+                str[index1] = rot13key[index2];
+                break;
+            }
+        }
+    }
+
+    return (str);
 }
+ 
