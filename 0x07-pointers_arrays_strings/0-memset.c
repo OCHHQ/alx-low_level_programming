4948d85fb463da2415c09e496b71179883e979d6
OCHHQ <enosejecollins@gmail.com>
2024-01-08 21:12:59 -0800

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _memset - fills a block of memory with a specific value
+ * @s: pointer to the memory area to be filled
+ * @b: the constant byte to fill the memory with
+ * @n: number of bytes to fill
+ *
+ * Return: a pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	for (; n > 0; i++)
+	{
+		st[i] = b;
+		n--;
+	}
+	return (s);
+}
