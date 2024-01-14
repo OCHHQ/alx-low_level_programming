6583242888621840f003703dc1d981eecc58fe62
OCHHQ <enosejecollins@gmail.com>
2024-01-14 08:49:01 -0800

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Entry point of the program
+ * @argc: The number of command-line arguments
+ * @argv: An array containing the command-line arguments
+ *
+ * Return: Always 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
