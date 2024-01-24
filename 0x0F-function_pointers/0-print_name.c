ebe411acec89a40ba21ae3231dc008a40d9c70b1
OCHHQ <enosejecollins@gmail.com>
2024-01-24 07:04:21 -0800

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+/**
+ * print_name - a function that prints name
+ *
+ * @f: variable holding a string name
+ * @name: name of the person
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	
+	f(name);
+}
