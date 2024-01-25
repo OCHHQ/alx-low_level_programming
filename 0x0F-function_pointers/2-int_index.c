fce1b5c68c1b3078552666bb357c8e7876dfcb13
OCHHQ <enosejecollins@gmail.com>
2024-01-25 04:17:18 -0800

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -7,16 +7,17 @@
  *
  * Return: int elemet match, -1 (*cmp)(int)
  */
+int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
 	if (array == NULL || cmp == NULL)
 		return (-1);
 
-	for (i = 0; < size i++)
+	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]) != 0)
-			return (1);
+			return (i);
 	}
 	return (-1);
 }
